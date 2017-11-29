df73146388382f102f4d00d26ef380a35877d04f
Starlight Romero <starlightromero@gmail.com>
2017-11-29 17:42:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,18 @@ function destructivelyRemoveFirstKitten() {
   kittens.shift()
 }
 
-
 function appendKitten(name) {
   return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  
+}
+
+function removeFirstKitten() {
+  return kittens.slice(0)
 }
